284eb9d4b6812d6e2a4cbf5caa0f90dbf6433ac0
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-04-15 09:29:45 +0200

    Solution à la quête Angular10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CocktailService } from '../cocktail.service';
+
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.css']
+})
+export class CocktailListComponent implements OnInit {
+  public cocktails = this.cocktailService.getCocktails();
+  constructor(public cocktailService: CocktailService) {
+  }
+
+  ngOnInit() {
+  }
+
+}
