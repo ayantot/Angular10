621295eb5cc7a29e9249c07143ed58cfe1ce0b56
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-05-03 14:32:44 +0200

    Add Angular quest 12 WCS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CocktailService } from './cocktail.service';
 import { Component } from '@angular/core';
+import {NasaService} from './cocktail-list/nasa.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Cocktail list';
 
-  constructor(public CocktailService: CocktailService) {
+  image:string[];
 
+  constructor(private myService:NasaService){
+    this.image = [];
+
+    
   }
+
+  ngOnInit() {
+    this.myService.getImage().subscribe(
+      (param_image:string[]) => {
+        this.image = param_image;
+        document.body.style.backgroundImage = 'url("'+param_image[0]+'")';
+      }
+    );
+  }
+
 }
+
+
